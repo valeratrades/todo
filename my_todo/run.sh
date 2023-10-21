fe6b7cf13a10b5a962a3ac5e4c27e9183df82015
Valera6 <v79166789533@gmail.com>
2023-10-21 23:48:11 +0000

    run.sh

---
+++ my_todo/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Following is a standard set of rust run shortcuts
+
+alias b="cargo build --release"
+alias r="cargo run"
+alias t="cargo test"
+
+# f for full
+alias f="cargo build --release && my_todo"
