b37cd38bc5ff2c2aaee487dceb2740330b065c4b
Valera6 <v79166789533@gmail.com>
2023-10-21 23:36:52 +0000

    my_todo; minor

--- functions.sh
+++ functions.sh
@@ -2,19 +2,34 @@
 # Note that we're using some shortcuts from .bashrc. However, no env needs to be imported, as it happens automatically when we are sorced from .bashrc
 
 todo() {
-  v ~/Todo
+  e ~/Todo
 }
 
 tadd() {
-  mkfile "${HOME}/Todo/${1}.md"
+  mkp "${HOME}/Todo/${1}.md"
 }
 
 taddo() {
-  mkfile "${HOME}/Todo/${1}.md"
-  v "${HOME}/Todo/${1}.md"
+  mkp "${HOME}/Todo/${1}.md"
+  e "${HOME}/Todo/${1}.md"
 }
 
 tder() {
-  mkfile "${HOME}/Todo/${1}/main.md"
-  v "${HOME}/Todo/${1}/main.md"
+  mkp "${HOME}/Todo/${1}/main.md"
+  e "${HOME}/Todo/${1}/main.md"
 }
+
+## `my_todo` stuff
+tstart() {
+	my_todo start "$@" &
+}
+tdone() {
+	my_todo done
+}
+tfailed() {
+	my_todo failed
+}
+tkill() {
+	pkill -f my_todo
+}
+#
--- my_todo/src/main.rs
+++ my_todo/src/main.rs
@@ -1,13 +1,18 @@
-#[allow(dead_code, unused_imports)]
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::process::Command;
 
 mod _static {
-	pub static CATEGORIES: [&str; 4] = ["d:data-collection", "h:home-chore", "w:workout", "i:close-git-issue"];
+	pub static CATEGORIES: [&str; 7] = ["", "d:data-collection", "h:home-chore", "w:workout", "i:close-git-issue", "t:tooling", "ev:day's-ev-calculation"];
 	pub static HARD_STOP_COEFF: f32 = 1.5;
+	pub static STATE_PATH: &str = "/home/v/tmp/my_todo/ongoing.json";
+	pub static SAVE_PATH: &str = "/home/v/data/my_todo/results.json";
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Category(String);
 impl From<String> for Category {
 	fn from(c: String) -> Self {
@@ -19,7 +24,7 @@ impl From<String> for Category {
 				return Category(s.to_owned());
 			}
 			let split: Vec<&str> = category.split(':').collect();
-			if split[0] == s || split[1] == s {
+			if split.get(0) == Some(&s) || split.get(1) == Some(&s) {
 				return Category(category.to_string());
 			}
 		}
@@ -28,64 +33,149 @@ impl From<String> for Category {
 		std::process::exit(1);
 	}
 }
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Ongoing {
-	timestamp: i64,
+	timestamp_s: u32,
 	category: Category,
 	estimated_minutes: u32,
+	description: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-struct Record {
-	timestamp: i64,
+#[derive(Debug, Deserialize, Serialize)]
+struct Result {
+	timestamp_s: u32,
 	category: Category,
 	estimated_minutes: u32,
-	status: bool,
-	realised_time: u32,
+	description: String,
+	completed: bool,
+	realised_minutes: u32,
 }
 
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
+	if args.len() == 1 {
+		// try to pick up a dropped task
+		run();
+	}
 	match args.get(1).map(String::as_str) {
 		Some("start") => start(&args),
 		Some("done") => done(),
 		Some("failed") => failed(),
 		Some(cmd) if cmd.replace("-", "").starts_with('h') => help(),
-		_ => panic!("No such option: {}", args.get(1).unwrap_or(&String::new())),
+		_ => {
+			eprintln!("\x1b[31mNo such option: {}\x1b[0m\nConsult \x1b[34m--help\x1b[0m", args.get(1).unwrap_or(&String::new()));
+			std::process::exit(1);
+		}
 	}
 }
 
 fn start(args: &[String]) {
-	let mut estimated_minutes: u32 = 0;
-	let mut arg_category = String::new();
-	for arg in args.iter().skip(2) {
-		if let Ok(num) = arg.parse::<u32>() {
-			if num > 90 {
-				println!("Error: Cut your task into smaller parts. Anything above 90m does not make sense.");
+	let args: &[String] = &args[2..]; // for simpler count
+
+	let estimated_minutes: u32 = {
+		if args.len() >= 1 {
+			if let Ok(num) = args[0].parse::<u32>() {
+				if num > 90 {
+					eprintln!("\x1b[31mCut your task into smaller parts. Anything above 90m does not make sense.\x1b[0m");
+					std::process::exit(1);
+				}
+				num
+			} else {
+				eprintln!("\x1b[31mFirst argument after action must be estimated time in minutes\x1b[0m");
 				std::process::exit(1);
 			}
-			estimated_minutes = num;
 		} else {
-			arg_category = arg.to_string();
+			90_u32
 		}
-	}
-	let category = Category::from(arg_category);
-	let timestamp = Utc::now().timestamp_millis();
-	let entry = Ongoing {
-		timestamp,
-		category,
-		estimated_minutes,
+	};
+	let arg_category: String = {
+		if args.len() >= 2 {
+			args[1].to_string()
+		} else {
+			"".to_owned()
+		}
+	};
+	let description: String = {
+		if args.len() >= 3 {
+			args[2].to_string()
+		} else {
+			"".to_owned()
+		}
 	};
 
-	write_and_time(entry);
+	let category = Category::from(arg_category);
+	let timestamp_s = Utc::now().timestamp() as u32;
+	let task = Ongoing {
+		timestamp_s,
+		category,
+		estimated_minutes,
+		description,
+	};
+
+	let parent_dir = std::path::Path::new(&_static::STATE_PATH).parent().unwrap();
+	let _ = std::fs::create_dir_all(parent_dir);
+	let mut file = File::create(&_static::STATE_PATH).unwrap();
+	let serialized = serde_json::to_string(&task).unwrap();
+	file.write_all(serialized.as_bytes()).unwrap();
+
+	run();
 }
 
+fn save_result(mut completed: bool) {
+	let mut file = File::open(&_static::STATE_PATH).unwrap();
+	let mut contents = String::new();
+	file.read_to_string(&mut contents).unwrap();
+	let ongoing: Ongoing = serde_json::from_str(&contents).unwrap();
+
+	let realised_minutes = {
+		let diff_m = ((Utc::now().timestamp() as u32 - ongoing.timestamp_s) as f32 / 60.0) as u32;
+		let hard_stop_m = (&_static::HARD_STOP_COEFF * ongoing.estimated_minutes as f32 + 0.5) as u32;
+		if hard_stop_m < diff_m {
+			completed = false; // It was possible to do `my_todo done` while executable is inactive, passing completed==true here, while far pasts the hard stop
+			hard_stop_m
+		} else {
+			diff_m
+		}
+	};
+	let result = Result {
+		timestamp_s: ongoing.timestamp_s,
+		category: ongoing.category,
+		estimated_minutes: ongoing.estimated_minutes,
+		description: ongoing.description,
+		completed,
+		realised_minutes: realised_minutes,
+	};
+
+	let parent_dir = std::path::Path::new(&_static::SAVE_PATH).parent().unwrap();
+	let _ = std::fs::create_dir_all(parent_dir);
+	let mut results: VecDeque<Result> = match File::open(&_static::SAVE_PATH) {
+		Ok(mut file) => {
+			let mut contents = String::new();
+			file.read_to_string(&mut contents).unwrap();
+			serde_json::from_str(&contents).unwrap_or_else(|_| VecDeque::new())
+		}
+		Err(_) => VecDeque::new(),
+	};
+
+	results.push_back(result);
+
+	let mut file = File::create(&_static::SAVE_PATH).unwrap();
+	file.write_all(serde_json::to_string(&results).unwrap().as_bytes()).unwrap();
+	let _ = std::fs::remove_file(&_static::STATE_PATH);
+
+	std::thread::sleep(std::time::Duration::from_millis(300)); // wait for eww to process previous request if any.
+	let eww_output = Command::new("sh").arg("-c").arg("eww get todo_timer".to_owned()).output().unwrap();
+	let todo_timer = String::from_utf8_lossy(&eww_output.stdout).trim().to_string();
+	if !todo_timer.starts_with("Out") {
+		let _ = Command::new("sh").arg("-c").arg("eww update todo_timer=None".to_owned()).output().unwrap();
+	}
+}
 fn done() {
-	println!("done")
+	save_result(true);
 }
 
 fn failed() {
-	println!("failed")
+	save_result(false);
 }
 
 fn help() {
@@ -94,27 +184,77 @@ fn help() {
 	println!(
 		r#"Start a task with timer, then store error (to track improvement of your estimations of time spent on different task categories)
 	Usage:
-		my_todo start [EXPECTED_TIME] [CATEGORY]
+		my_todo [ACTION] [EXPECTED_TIME] [CATEGORY] [DESCRIPTION]
+	Example Usage:
+		my_todo start 20 tooling gtk-default-browser
 		. . . // start doing the task, then:
-		my_todo done || my_todo failed
-	Commands:
+		my_todo done
+	Actions:
 		start   start a timer for a task
 		done    mark the task as done and save the time it took to finish  
 		failed  abort the task
+	Defaults:
+		if not provided, arguments default to:
+		[EXPECTED_TIME]: 90
+		[CATEGORY]: None
+		[DESCRIPTION]: ""
 	Arguments:
 		[EXPECTED_TIME]  Time in minutes that you expect the task to take.
 		[CATEGORY]       Category of the task. Can be one of:
-			{}"#,
-		categories.join(",\n    ")
+			{}
+		[DESCRIPTION]    Short descrption of the task,that will show up next to the timer."#,
+		categories.join(",\n\t\t\t")
 	);
 }
 
 // ----------------------------------------------------------------------------
 
-fn write_and_time(task: Ongoing) {
-	std::fs::create_dir_all("/home/v/data/my_todo");
-	let estimated_ms = Ongoing::timestamp + Ongoing::estimated_minutes as i64 * 60 * 1000;
-	let hard_stop_ms = Ongoing::timestamp + (&_static.HARD_STOP_COEFF * (Ongoing::estimated_minutes * 60 * 1000) as f32) as i64;
+fn run() {
+	let task: Ongoing = {
+		if std::path::Path::new(&_static::STATE_PATH).exists() {
+			let mut file = File::open(&_static::STATE_PATH).unwrap();
+			let mut contents = String::new();
+			file.read_to_string(&mut contents).unwrap();
+			serde_json::from_str(&contents).unwrap()
+		} else {
+			eprintln!("No record of an ongoing task found, exiting.");
+			std::process::exit(0);
+		}
+	};
+	let estimated_s = task.timestamp_s + task.estimated_minutes * 60;
+	let hard_stop_s = task.timestamp_s + (&_static::HARD_STOP_COEFF * (task.estimated_minutes * 60) as f32) as u32;
 
-	dbg!(&hard_stop_ms);
+	loop {
+		let path = std::path::Path::new(&_static::STATE_PATH);
+		if !path.exists() {
+			break;
+		}
+
+		let now_s = Utc::now().timestamp();
+		let e_diff = estimated_s as i64 - now_s as i64;
+		let h_diff = hard_stop_s as i64 - now_s as i64;
+		let description = {
+			if task.description.as_str() == "" {
+				format!("")
+			} else {
+				format!("_{}", task.description)
+			}
+		};
+
+		let value = if h_diff < 0 {
+			format!("Out{}", description)
+		} else if e_diff < 0 {
+			format!("-{:02}:{:02}{}", h_diff / 60, h_diff % 60, description)
+		} else {
+			format!("{:02}:{:02}{}", e_diff / 60, e_diff % 60, description)
+		};
+
+		//NB: will break if the passed String has spaces in it.
+		let _ = Command::new("sh").arg("-c").arg(format!("eww update todo_timer={}", value)).output().unwrap();
+		if value.starts_with("Out") {
+			save_result(false);
+			std::process::exit(0);
+		}
+		std::thread::sleep(std::time::Duration::from_secs(1));
+	}
 }
