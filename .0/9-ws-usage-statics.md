504f80dc7164e0824f555f842e201fb9ae3f3823
Valera6 <v79166789533@gmail.com>
2023-12-30 21:06:18 +0000

    .

--- .0/9-ws-usage-statics.md
+++ .0/9-ws-usage-statics.md
@@ -1,6 +1,7 @@
 1. just ws stats
 - to get current: `swaymsg -t get_workspaces | jq -r '.[] | select(.focused==true).name'`
 1. if chrome, get name of the tab
+- swaymsg -t get_tree | jq -r '.. | (.nodes? // empty)[] | select(.focused==true)'
 1. integrate with daily_ev
 1. make an android app that sends me digital wellbeing stats
 1. connect the data stream to the app
--- activity_monitor/src/main.rs
+++ activity_monitor/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+	let mut prev_activity = String::new();
+	loop {
+		let activity = get_activity();
+		if prev_activity != activity {
+			println!("{}", activity);
+			prev_activity = activity;
+		}
+		std::thread::sleep(std::time::Duration::from_secs(1));
+	}
+}
+
+fn get_activity() -> String {
+	let output = std::process::Command::new("sh")
+		.arg("-c")
+		.arg("swaymsg -t get_workspaces | jq -r '.[] | select(.focused==true).name'")
+		.output()
+		.unwrap();
+	let output_str = String::from_utf8_lossy(&output.stdout);
+	let focused = output_str.trim_end_matches('\n');
+
+	//if focused == "2".to_owned()
+
+	let activity: String = match focused.parse() {
+		Ok(1) => "neovim".to_owned(),
+		//TODO!!!!: get the tab
+		Ok(2) => "googling".to_owned(),
+		Ok(3) => "editing todos/notes".to_owned(),
+		//TODO!: get which one
+		Ok(4) => "social networks".to_owned(),
+		Ok(5) => "reading a book".to_owned(),
+		Ok(num) => format!("ws{}", num),
+		Err(_) => unreachable!(),
+	};
+
+	activity
 }
