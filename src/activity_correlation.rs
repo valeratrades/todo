53a23f0f5f3a940db8d2855d8a67074e0e144eed
Valera6 <v79166789533@gmail.com>
2024-01-05 20:35:54 +0000

    .

--- src/activity_correlation.rs
+++ src/activity_correlation.rs
@@ -1,5 +1,7 @@
 use crate::activity_monitor::Total;
 
+//TODO!!!!!!!!!!!!: when enough data is gathered, do the correlations.
+
 //1. deserialize every file in totals directory
 
 //2. for every total of the structure, correlation against the recorded ev of the same day (if no ev, skip).
