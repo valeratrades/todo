a088b665c5aabd3d5cf89b16e5f21f5d25107855
Valera6 <v79166789533@gmail.com>
2024-01-08 23:28:33 +0000

    .

--- functions.sh
+++ functions.sh
@@ -1,28 +1,2 @@
-local pull() {
-	( git -C "$TODO_PATH" pull > /dev/null 2>&1 & ) & disown
-}
-local push() {
-	( git -C "$TODO_PATH" add -A && git -C "$TODO_PATH" commit -m "." && git -C "$TODO_PATH" push ) > /dev/null 2>&1 & disown
-}
-
-
-## `my_todo` stuff
-tstart() {
-	my_todo start "$@" &
-}
-tdone() {
-	my_todo done
-}
-tfailed() {
-	my_todo failed
-}
-tkill() {
-	pkill -f my_todo
-}
-tedit() {
-	e ${HOME}/data/personal/todo.json
-}
-
-#==============================================================================
-
 alias tev="todo manual --ev"
+alias tdo="cs ${HOME}/.data/personal/"
