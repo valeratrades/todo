b4d75fe19c2eb02b308141b39f25afd3a5888bab
Valera6 <v79166789533@gmail.com>
2024-04-13 00:00:35 +0000

    .

--- src/manual_stats.rs
+++ src/manual_stats.rs
@@ -68,7 +68,7 @@ pub fn update_or_open(config: Config, args: ManualArgs) -> Result<()> {
 				evening: Evening::default(),
 				sleep: Sleep::default(),
 				non_negotiables_done: 0,
-				jo_mins: JoTimes::default(),
+				jo_mins: JoMins::default(),
 				number_of_NOs: 0,
 			}
 		}
