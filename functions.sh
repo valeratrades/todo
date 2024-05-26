e8f80ce1d6353a07555644f1a5c7724357bfb497
Valera6 <v79166789533@gmail.com>
2024-05-26 00:39:05 +0000

    _

--- functions.sh
+++ functions.sh
@@ -1,6 +1,5 @@
 alias tm="todo manual"
-alias teva="todo manual ev -a"
-alias tevs="todo manual ev -s"
+# can't make alaises for `tm ev -a` and `tm ev -s` because of possibility of `-dx` flag. And they are short enough already, calm down.
 alias tdo="cs ${HOME}/.data/personal/"
 alias tstart="todo timer start"
 alias tdone="todo timer done"
