d5623a2fdd2968372e45b8a22cadf9a694f83543
Valera6 <v79166789533@gmail.com>
2024-06-12 17:21:18 +0000

    _

--- src/manual_stats.rs
+++ src/manual_stats.rs
@@ -339,7 +339,7 @@ impl Day {
 		}
 
 		let new_cw_counter = self.counters.cargo_watch;
-		conditional_update(&mut pbs_as_value, "cw_counter", new_cw_counter, |new, old| new > old);
+		conditional_update(&mut pbs_as_value, "cw_counter", new_cw_counter, |new, old| new >= old + 10);
 
 		// Returns bool for convienience of recursing some of these
 		let mut streak_update = |metric: &str, condition: &dyn Fn(&Day) -> bool| -> bool {
