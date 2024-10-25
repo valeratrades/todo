24b0b5df0e8ea88a394187230e4423352348f400
valeratrades <v79166789533@gmail.com>
2024-10-25 20:15:41 +0200

    fixup!

--- default.nix
+++ default.nix
@@ -6,7 +6,9 @@ pkgs.rustPlatform.buildRustPackage rec {
   pname = manifest.name;
   version = manifest.version;
 	
-	buildInputs = with pkgs; [openssl pkg-config];
+	buildInputs = with pkgs; [pkg-config openssl openssl.dev]; # rustPlatform.bindgenHook];
+	nativeBuildInputs = with pkgs; [pkg-config];
+	env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
