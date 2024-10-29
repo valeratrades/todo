7c0f9e56bca40247ce7110b0fada02204f2f9b17
valeratrades <v79166789533@gmail.com>
2024-10-29 14:21:59 +0000

    wip

--- flake.nix
+++ flake.nix
@@ -1,32 +1,48 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (system: let
-        pkgs = pkgsFor.${system};
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-      in {
-        default = pkgs.rustPlatform.buildRustPackage rec {
-          pname = manifest.name;
-          version = manifest.version;
-
-          buildInputs = with pkgs; [
-            openssl
-            openssl.dev
+  outputs = inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        lib,
+        pkgs,
+        system,
+        config,
+        ...
+      }: 
+      {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (inputs.fenix.overlays.default)
           ];
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-
-          cargoLock.lockFile = ./Cargo.lock;
-          src = pkgs.lib.cleanSource ./.;
         };
-      });
+
+        devShells.default = with pkgs; let 
+        toolchain = pkgs.fenix.complete.withComponents [
+          "rustc"
+          "cargo"
+          "clippy"
+        ];
+        in mkShell
+        {
+          packages = with pkgs; [
+						hello
+            openssl
+            rust-analyzer-nightly
+            toolchain
+						pkg-config
+          ];
+        };
+      };
     };
 }
