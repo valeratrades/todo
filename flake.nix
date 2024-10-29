54db50872a736b045f434c4a34436057578ea87e
valeratrades <v79166789533@gmail.com>
2024-10-29 10:05:23 +0000

    fixup!

--- flake.nix
+++ flake.nix
@@ -17,12 +17,12 @@
           pname = manifest.name;
           version = manifest.version;
 
-          #buildInputs = with pkgs; [
-          #  openssl
-          #  openssl.dev
-          #];
-          #nativeBuildInputs = with pkgs; [ pkg-config ];
-          #env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+          buildInputs = with pkgs; [
+            openssl
+            openssl.dev
+          ];
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
           cargoLock.lockFile = ./Cargo.lock;
           src = pkgs.lib.cleanSource ./.;
