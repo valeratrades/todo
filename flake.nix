a3ce5be03ee7d601b7c786bfe9f5c9b49e17002f
valeratrades <v79166789533@gmail.com>
2024-10-29 15:37:02 +0000

    fixup!

--- flake.nix
+++ flake.nix
@@ -29,6 +29,27 @@
             ];
           };
 
+          packages =
+            let
+              manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+            in
+            {
+              default = pkgs.rustPlatform.buildRustPackage rec {
+                pname = manifest.name;
+                version = manifest.version;
+
+                buildInputs = with pkgs; [
+                  openssl
+                  openssl.dev
+                ];
+                nativeBuildInputs = with pkgs; [ pkg-config ];
+                env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+                cargoLock.lockFile = ./Cargo.lock;
+                src = pkgs.lib.cleanSource ./.;
+              };
+            };
+
           devShells.default =
             with pkgs;
             let
