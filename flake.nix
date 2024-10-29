b175c0626bbb59987e427a3c8e11b3cc123062ea
valeratrades <v79166789533@gmail.com>
2024-10-29 14:50:07 +0000

    fix: fucking nix

--- flake.nix
+++ flake.nix
@@ -9,40 +9,44 @@
     fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, ... }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = {
-        lib,
-        pkgs,
-        system,
-        config,
-        ...
-      }: 
-      {
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (inputs.fenix.overlays.default)
-          ];
-        };
-
-        devShells.default = with pkgs; let 
-        toolchain = pkgs.fenix.complete.withComponents [
-          "rustc"
-          "cargo"
-          "clippy"
-        ];
-        in mkShell
+      perSystem =
         {
-          packages = with pkgs; [
-						hello
-            openssl
-            rust-analyzer-nightly
-            toolchain
-						pkg-config
-          ];
+          lib,
+          pkgs,
+          system,
+          config,
+          ...
+        }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (inputs.fenix.overlays.default)
+            ];
+          };
+
+          devShells.default =
+            with pkgs;
+            let
+              toolchain = pkgs.fenix.complete.withComponents [
+                "rustc"
+                "cargo"
+                "clippy"
+              ];
+            in
+            mkShell {
+              packages = with pkgs; [
+                openssl
+                rust-analyzer-nightly
+                toolchain
+                pkg-config
+              ];
+              LD_LIBRARY_PATH = lib.makeLibraryPath [ pkgs.openssl ];
+            };
         };
-      };
     };
 }
