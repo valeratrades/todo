aa71e9a362e531785a51124915eeb26a471937f4
valeratrades <v79166789533@gmail.com>
2024-11-29 10:40:37 +0000

    chore: handle borders parsing properly

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use chrono::Duration;
 #[cfg(not(test))]
 use chrono::Utc;
-
+use std::str::FromStr;
 use crate::config::AppConfig;
 #[cfg(test)]
 use crate::mocks::Utc;
+use color_eyre::eyre::{Report, Result, bail};
 
 pub fn format_date(days_back: usize, config: &AppConfig) -> String {
 	let date = Utc::now() - Duration::days(days_back as i64);
@@ -13,17 +14,36 @@ pub fn format_date(days_back: usize, config: &AppConfig) -> String {
 	(date - offset).format(config.date_format.as_str()).to_string()
 }
 
+/// Ends of each day-section as offset to wake-time
+#[derive(Clone, Debug, Default, derive_new::new, Copy)]
+pub struct DaySectionBorders {
+	pub morning_end: f32,
+	pub day_end: f32,
+	pub evening_end: f32,
+}
+impl std::str::FromStr for DaySectionBorders {
+	type Err = Report;
+	fn from_str(borders_str: &str) -> Result<Self> {
+		let mut vec_offsets = Vec::with_capacity(3);
+		for s in borders_str.split(":").into_iter() {
+			vec_offsets.push(s.parse::<f32>()?);
+		}
+		if vec_offsets.len() == 3 {
+			Ok(Self { morning_end: vec_offsets[0], day_end: vec_offsets[1], evening_end: vec_offsets[2] })
+		} else {
+			bail!("invalid dimensions");
+		}
+	}
+}
+
+
 /// Diff of sleep time from 00:00 utc
 pub fn same_day_buffer() -> chrono::TimeDelta {
 	let waketime = std::env::var("WAKETIME").unwrap();
 	let waketime = chrono::NaiveTime::parse_from_str(waketime.as_str(), "%H:%M").unwrap();
 
-	// I don't know what happened here, but I was getting the "used while borrowed"
-	let sleep_offset = std::env::var("DAY_SECTION_BORDERS").unwrap();
-	let sleep_offset = sleep_offset.split(":").collect::<Vec<&str>>();
-	let sleep_offset = sleep_offset.last().unwrap();
-	let sleep_offset = sleep_offset.parse::<f64>().unwrap();
-	let sleep_offset = chrono::Duration::minutes((sleep_offset * 60.0) as i64);
+	let borders = DaySectionBorders::from_str(&std::env::var("DAY_SECTION_BORDERS").unwrap()).unwrap();
+	let sleep_offset = chrono::Duration::minutes((borders.evening_end * 60.0) as i64);
 
 	let bedtime = waketime + sleep_offset;
 	let new_day = bedtime + chrono::Duration::hours(6);
@@ -37,9 +57,9 @@ mod tests {
 	use super::*;
 	use crate::config::AppConfig;
 
-	fn init_test(waketime: &str, day_section_borders: &str, t: Option<(i32, u32, u32, u32, u32, u32)>) -> AppConfig {
-		std::env::set_var("WAKETIME", waketime);
-		std::env::set_var("DAY_SECTION_BORDERS", day_section_borders);
+	fn init_test(t: Option<(i32, u32, u32, u32, u32, u32)>) -> AppConfig {
+		std::env::set_var("WAKETIME", "05:00");
+		std::env::set_var("DAY_SECTION_BORDERS", "2.5:10:16");
 
 		if let Some(t) = t {
 			let mock_now = chrono::Utc.with_ymd_and_hms(t.0, t.1, t.2, t.3, t.4, t.5).unwrap();
@@ -54,7 +74,7 @@ mod tests {
 
 	#[test]
 	fn test_same_day_buffer() {
-		let _config = init_test("05:00", "16", Some((2024, 5, 29, 12, 0, 0)));
+		let _ = init_test(Some((2024, 5, 29, 12, 0, 0)));
 		let offset = same_day_buffer();
 
 		assert_eq!(offset, chrono::Duration::hours(3).checked_add(&chrono::Duration::minutes(0)).unwrap());
@@ -62,7 +82,7 @@ mod tests {
 
 	#[test]
 	fn test_format_date() {
-		let config = init_test("07:00", "22.5", Some((2024, 5, 29, 12, 0, 0)));
+		let config = init_test(Some((2024, 5, 29, 12, 0, 0)));
 
 		let formatted_date = format_date(1, &config);
 		assert_eq!(formatted_date, "2024-05-28");
@@ -70,11 +90,11 @@ mod tests {
 
 	#[test]
 	fn test_correct_day() {
-		let config = init_test("05:00", "16", Some((2024, 5, 29, 2, 59, 0)));
+		let config = init_test(Some((2024, 5, 29, 2, 59, 0)));
 		let formatted_date = format_date(0, &config);
 		assert_eq!(formatted_date, "2024-05-28");
 
-		let config = init_test("05:00", "16", Some((2024, 5, 29, 3, 1, 0)));
+		let config = init_test(Some((2024, 5, 29, 3, 1, 0)));
 		let formatted_date = format_date(0, &config);
 
 		assert_eq!(formatted_date, "2024-05-29");
