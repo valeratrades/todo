84f6733adeff652b528ef739faae34fcd6ac7b14
valeratrades <v79166789533@gmail.com>
2024-12-21 02:47:52 +0000

    refactor: 2w-sprints initial adaptation

--- src/todos.rs
+++ src/todos.rs
@@ -9,21 +9,19 @@ use std::{
 };
 
 use clap::Args;
-use color_eyre::eyre::{eyre, Context as _, Report, Result};
+use color_eyre::eyre::{bail, eyre, Context as _, Report, Result};
 use tempfile::Builder;
 use v_utils::io::OpenMode;
 
 use crate::{
 	config::AppConfig,
-	day_section::DaySection,
 	manual_stats::{Day, Repercussions},
 	utils,
 };
 
 #[deprecated(note = "In the process of rewriting to work with 2w-splits instead")]
 pub fn compile_quickfix(config: AppConfig) -> Result<()> {
-	let day_section = DaySection::build().unwrap();
-	let path: PathBuf = day_section_path(&config, &day_section);
+	let path: PathBuf = section_path(&config, todo!())?;
 
 	let mut tasks: Vec<Task> = Vec::new();
 
@@ -76,7 +74,8 @@ Clear separation between tasks below 5m and above. Those below can be done whene
 With current state of the day, the following repercussions will be applied:
 {}
 "#,
-		day_section.description(),
+		//day_section.description(),
+		todo!(),
 		repercussions
 	));
 
@@ -93,9 +92,7 @@ With current state of the day, the following repercussions will be applied:
 }
 
 pub fn open_or_add(config: AppConfig, flags: TodosFlags, name: Option<String>) -> Result<()> {
-	let day_section = flags.extract_day_section();
-
-	let mut path = day_section_path(&config, &day_section);
+	let mut path = section_path(&config, &flags)?;
 
 	if let Some(name) = name {
 		path.push([&name, ".md"].concat());
@@ -123,54 +120,111 @@ pub struct AddArgs {
 }
 #[derive(Args)]
 pub struct TodosFlags {
+	/// Add tasks to named section of `sprints` (Mutually exclusive with `conditional`). Default is `other`.
 	#[arg(long, short)]
-	pub morning: bool,
+	sprints: Option<String>,
+	/// Add tasks to named section of `conditional` (Mutually exclusive with `sprints`). Default is `other`.
+	//TODO!!!!: shouldn't require `<importance>-<difficulty>` prefix, on the contrary - here should error if it's provided.
 	#[arg(long, short)]
-	pub work: bool,
+	conditional: bool,
+	/// Create the parenting section if it doesn't exist.
 	#[arg(long, short)]
-	pub evening: bool,
-	#[arg(long, short)]
-	pub night: bool,
+	parents: bool,
+	/// Open the file after creating it.
 	#[arg(long, short)]
 	pub open: bool,
 }
 
-impl TodosFlags {
-	fn extract_day_section(&self) -> DaySection {
+fn section_path<'a>(config: &AppConfig, flags: &TodosFlags) -> Result<PathBuf> {
+	let todos_dir = config.todos.path.clone();
+
+	let from_root = match (flags.sprints.as_ref(), flags.conditional) {
+		(Some(name), false) => name,
+		(None, true) => "conditional",
+		(None, false) => "other",
+		(Some(_), true) => bail!("Flags `sprints` and `conditional` are mutually exclusive."),
+	};
+
+	match std::fs::metadata(todos_dir.join(from_root)).is_ok() {
+		true => Ok(todos_dir.join(from_root)),
+		false => match flags.parents {
+			true => {
+				let path = todos_dir.join(from_root);
+				std::fs::create_dir_all(&path)?;
+				Ok(path)
+			}
+			false => bail!("Section '{from_root}' does not exist. Add `-p` flag to automatically create it."),
+		},
+	}
+}
+
+#[derive(Debug, Clone)]
+enum TaskMeta {
+	Simple {
+		name: String,
+	},
+	Full {
+		importance: u8, // wanted to go with 16 hexadecimal digits, but that would prevent having things sorted for fre.
+		est_hours: f32,
+		difficulty_density: u8,
+		name: String, //? Do I need this?
+	},
+}
+impl TaskMeta {
+	pub fn try_parse_full(name: &str) -> Result<Self> {
+		let split: Vec<_> = name.split('-').collect();
+
+		let formatting_error: String = format!("Error: Incorrect Task Format\nWant: \"3-4-my-task.md\"\nGot: {}", name);
+
+		// [1] can be longer than 1 char; total number of dashes can be more than 4, as we don't control actual contents, - don't check those.
+		if split.len() < 4 || split[0].len() != 1 || split[2].len() != 1 {
+			bail!(formatting_error.clone());
+		}
+
+		let importance: u8 = split[0].parse().with_context(|| formatting_error.clone())?;
+		let est_hours: f32 = split[1].parse().with_context(|| formatting_error.clone())?;
+		let difficulty_density: u8 = split[2].parse().with_context(|| formatting_error.clone())?;
+		let name: String = split[2..split.len()].join(" ").trim_end_matches(".md").to_string();
+		Ok(TaskMeta::Full {
+			importance,
+			est_hours,
+			difficulty_density,
+			name,
+		})
+	}
+
+	//TODO!: start erroring on provision of `full` metadata here, to prevent habitual user logical errors. (? do I care though?)
+	pub fn parse_simple(name: &str) -> Self {
+		let name = name.trim_end_matches(".md").to_string();
+		TaskMeta::Simple { name }
+	}
+}
+impl TaskMeta {
+	//HACK: old way of calcing this, hardly useful as of now.
+	fn priority(&self) -> u8 {
 		match self {
-			Self { morning: true, .. } => DaySection::Morning,
-			Self { work: true, .. } => DaySection::Work,
-			Self { evening: true, .. } => DaySection::Evening,
-			Self { night: true, .. } => DaySection::Night,
-			_ => DaySection::Evening,
+			TaskMeta::Simple { .. } => 0,
+			TaskMeta::Full { importance, difficulty_density, .. } => importance * (10 - difficulty_density),
+		}
+	}
+
+	fn name(&self) -> &str {
+		match self {
+			TaskMeta::Simple { name } => name,
+			TaskMeta::Full { name, .. } => name,
 		}
 	}
 }
 
-fn day_section_path<'a>(config: &'a AppConfig, day_section: &'a DaySection) -> PathBuf {
-	let todos_dir = config.todos.path.clone();
-
-	let path_appendix: &str = match day_section.to_owned() {
-		DaySection::Morning => ".morning/",
-		DaySection::Work => ".work/",
-		DaySection::Evening => ".evening/",
-		DaySection::Night => ".night/",
-	};
-
-	todos_dir.join(path_appendix)
-}
-
-#[derive(Debug, Clone)]
-struct TaskSplit {
-	_importance: u8,
-	_difficulty: u8,
-	name: String,
-}
 #[derive(Debug, Clone)]
 struct Task {
-	priority: f32,
+	meta: TaskMeta,
 	path: PathBuf,
-	split: TaskSplit,
+}
+impl Task {
+	fn priority(&self) -> u8 {
+		self.meta.priority()
+	}
 }
 impl TryFrom<PathBuf> for Task {
 	type Error = Report;
@@ -181,31 +235,19 @@ impl TryFrom<PathBuf> for Task {
 			.ok_or_else(|| eyre!("Filename not found in path"))?
 			.to_str()
 			.ok_or_else(|| eyre!("Filename is not valid UTF-8"))?;
-		let split: Vec<_> = filename.split('-').collect();
 
-		let formatting_error: String = format!("Error: Incorrect Task Format\nWant: \"3-4-my-task.md\"\nGot: {}", filename);
-
-		if split.len() < 3 || split[0].len() != 1 || split[1].len() != 1 {
-			return Err(eyre!(formatting_error.clone()));
-		}
-
-		let importance: u8 = split[0].parse().with_context(|| formatting_error.clone())?;
-		let difficulty: u8 = split[1].parse().with_context(|| formatting_error.clone())?;
-		let name: String = split[2..split.len()].join(" ").trim_end_matches(".md").to_string();
-
-		let split = TaskSplit {
-			_importance: importance,
-			_difficulty: difficulty,
-			name,
+		let meta: TaskMeta = {
+			let p = path.parent().unwrap();
+			match p.to_str() {
+				Some("other") => TaskMeta::parse_simple(filename),
+				_ => match p.parent().unwrap().to_str() {
+					Some("sprints") => TaskMeta::try_parse_full(filename)?,
+					Some("conditional") => TaskMeta::parse_simple(filename),
+					_ => bail!("Error: Task in unknown section."),
+				},
+			}
 		};
-
-		let priority = importance * (10 - difficulty);
-
-		Ok(Task {
-			priority: priority.into(),
-			path,
-			split,
-		})
+		Ok(Task { path, meta })
 	}
 }
 fn quick_sort_tasks(arr: &mut [Task], low: isize, high: isize) {
@@ -222,11 +264,11 @@ fn partition_tasks(arr: &mut [Task], low: isize, high: isize) -> isize {
 
 	loop {
 		store_index += 1;
-		while arr[store_index as usize].priority < arr[pivot].priority {
+		while arr[store_index as usize].priority() < arr[pivot].priority() {
 			store_index += 1;
 		}
 		last_index -= 1;
-		while last_index >= 0 && arr[last_index as usize].priority > arr[pivot].priority {
+		while last_index >= 0 && arr[last_index as usize].priority() > arr[pivot].priority() {
 			last_index -= 1;
 		}
 		if store_index >= last_index {
@@ -252,7 +294,7 @@ impl Display for Task {
 {}
 
 "#,
-			self.split.name,
+			self.meta.name(),
 			self.path.display(),
 			inner_contents
 		)
