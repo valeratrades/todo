b792ad25260672cc6f021ce55609eaa9d0c85a2b
valeratrades <v79166789533@gmail.com>
2025-01-02 17:11:51 +0000

    ci: _

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
-		pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs = { nixpkgs, rust-overlay, flake-utils, pre-commit-hooks, ... }:
@@ -13,49 +13,46 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+            };
+          };
+        };
       in
       {
-				packages =
-					let
-						manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-					in
-						{
-						default = pkgs.rustPlatform.buildRustPackage rec {
-							pname = manifest.name;
-							version = manifest.version;
+        packages =
+          let
+            manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+          in
+          {
+            default = pkgs.rustPlatform.buildRustPackage rec {
+              pname = manifest.name;
+              version = manifest.version;
 
-							buildInputs = with pkgs; [
-								openssl
-								openssl.dev
-							];
-							nativeBuildInputs = with pkgs; [ pkg-config ];
-							env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+              buildInputs = with pkgs; [
+                openssl
+                openssl.dev
+              ];
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+              env.PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
 
-							cargoLock.lockFile = ./Cargo.lock;
-							src = pkgs.lib.cleanSource ./.;
-						};
-					};
-
-						checks = {
-					pre-commit-check = pre-commit-hooks.lib.${system}.run {
-						src = ./.;
-						hooks = {
-							nixpkgs-fmt.enable = true;
-						};
-					};
-				};
+              cargoLock.lockFile = ./Cargo.lock;
+              src = pkgs.lib.cleanSource ./.;
+            };
+          };
 
         devShells.default = with pkgs; mkShell {
-					stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+          inherit (checks.pre-commit-check) shellHook;
+          stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
           packages = [
-						mold-wrapped
+            mold-wrapped
             openssl
             pkg-config
             (rust-bin.fromRustupToolchainFile ./.cargo/rust-toolchain.toml)
           ];
-					 shellHook = ''
-            echo "Run pre-commit-check with: nix run .#pre-commit-check"
-          '';
         };
       }
     );
