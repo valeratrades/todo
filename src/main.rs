0233e81e5675f114aad7df2571f488decc473f1b
Valera <valeratrades@gmail.com>
2025-11-04 11:42:12 +0000

    nuke: legacy and unused

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,17 @@
 #![allow(clippy::len_zero)]
-mod activity_monitor;
 mod blocker;
 mod clockify;
 pub mod config;
-pub mod day_section;
 mod manual_stats;
 mod milestones;
 pub mod mocks;
 mod shell_init;
-mod todos;
 pub mod utils;
-use clap::{Args, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 use config::AppConfig;
 use v_utils::clientside;
 
 const MANUAL_PATH_APPENDIX: &str = "manual_stats/";
-const MONITOR_PATH_APPENDIX: &str = "activities_monitor/";
-const TOTALS_PATH_APPENDIX: &str = "activities_totals/";
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -29,29 +24,6 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-	/// Opens the target path
-	///  Ex
-	///```rust
-	///todo open -w
-	///```
-	Open(todos::OpenArgs),
-	/// Add a new task.
-	/// Every entry has the following format:
-	/// `{importance}-{difficulty}-{name}`,
-	///where:
-	///- importance: 0->9, the higher the more important
-	///- difficulty: 0->9, the higher the more difficult
-	///  Ex:
-	///```rust
-	///todo add 2-3-test -n
-	///```
-	Add(todos::AddArgs),
-	/// Compile list of first priority tasks based on time of day
-	///  Ex:
-	///```rust
-	///todo quickfix
-	///```
-	Quickfix(NoArgs),
 	/// Record day's ev and other stats.
 	///Following records ev of 420 for yesterday, then opens the file.
 	///```rust
@@ -60,8 +32,6 @@ enum Commands {
 	Manual(manual_stats::ManualArgs),
 	/// Operations with milestones (1d, 1w, 1M, 1Q, 1y)
 	Milestones(milestones::MilestonesArgs),
-	/// Start monitoring user activities
-	Monitor,
 	/// Shell aliases and hooks. Usage: `todos init <shell> | source`
 	Init(shell_init::ShellInitArgs),
 	/// Blockers tree
@@ -69,8 +39,6 @@ enum Commands {
 	/// Clockify time tracking
 	Clockify(clockify::ClockifyArgs),
 }
-#[derive(Args)]
-struct NoArgs {}
 
 fn main() {
 	clientside!();
@@ -86,15 +54,7 @@ fn main() {
 
 	// All the functions here can rely on config being correct.
 	let success = match cli.command {
-		Commands::Open(open_args) => {
-			let mut todos_flags = open_args.shared;
-			todos_flags.open = true;
-			todos::open_or_add(config, todos_flags, None)
-		}
-		Commands::Add(add_args) => todos::open_or_add(config, add_args.shared, Some(add_args.name)),
-		Commands::Quickfix(_) => todos::compile_quickfix(config),
 		Commands::Manual(manual_args) => manual_stats::update_or_open(config, manual_args),
-		Commands::Monitor => activity_monitor::start(config),
 		Commands::Milestones(milestones_command) => milestones::milestones_command(config, milestones_command),
 		Commands::Init(args) => {
 			shell_init::output(config, args);
