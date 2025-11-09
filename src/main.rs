5bcfb9b2bca7625965d8c0bb0c094d89024f42df
Valera <valeratrades@gmail.com>
2025-11-09 13:53:03 +0000

    fix: integration tests to respect their env boundaries
    fix: _

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod shell_init;
 pub mod utils;
 use clap::{Parser, Subcommand};
 use config::AppConfig;
-#[cfg(not(feature = "is_integration_test"))]
+#[cfg(not(test))]
 use v_utils::clientside;
 
 const MANUAL_PATH_APPENDIX: &str = "manual_stats/";
@@ -42,7 +42,7 @@ enum Commands {
 }
 
 fn main() {
-	#[cfg(not(feature = "is_integration_test"))]
+	#[cfg(not(test))]
 	clientside!();
 	let cli = Cli::parse();
 
